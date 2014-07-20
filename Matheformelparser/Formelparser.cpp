28b56253920c6bf7e1d4ef01b21cac7c1c862729
Linux User <linux@Ubuntu1304>
2014-07-20 13:13:59 +0200

    Java -> C++

--- Matheformelparser/Formelparser.cpp
+++ Matheformelparser/Formelparser.cpp
@@ -1,11 +1,15 @@
 // Includes - auf die Reihenfolge kommts an!
-# include <stdio.h>
-# include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <typeinfo>
+#include <cmath>
 
 //~ import java.io.IOException;
 //~ import java.io.StreamTokenizer;
 //~ import java.io.StringReader;
 //~ import java.lang.Math;
+
+using namespace std;
 
 #define FALSE 0
 #define TRUE  1
@@ -46,25 +50,25 @@
 
 // GLOBALE DEKLARATIONEN
 
-class FormelparserMinimal {
+class Formelparser {
     private:
 		int lookahead;										///< Vorschau auf Typ des nächsten Tokens
 		StreamTokenizer tokenizer;						///< Hilfsobjekt zur Aufsplittung der Eingabe in einzelne Token
 
-		String tokenToString(int token);
-		void next() throws IOException;
-		void match(int expected) throws IOException;
-		double parseExpr() throws IOException;
-		double parseTerm() throws IOException;
-		double parseFact() throws IOException;
-		double parseTrig() throws IOException;
+		string tokenToString(int token);
+		void next();
+		void match(int expected);
+		double parseExpr();
+		double parseTerm();
+		double parseFact();
+		double parseTrig();
 
     public:
 		int isValid;									///< Gibt an, ob der zuletzt geparste Ausdruck gültig gewesen ist
 		double parseResult;								///< enthält das Ergebnis des zuletzt geparsten Ausdruckes \sa isValid()
 
-		void FormelparserMinimal();
-		double parse(String s) throws IOException;
+		Formelparser();
+		double parse(string s);
 
 //    public class ParseException extends Exception {}		//* Funktioniert leider nicht
 };
@@ -73,22 +77,22 @@ class FormelparserMinimal {
 // UNTERPROGRAMME
 
 /// Hilfsfunktion für Debugausgaben
-String tokenToString(int token) {
+string Formelparser::tokenToString(int token) {
 	if (token == StreamTokenizer.TT_NUMBER) { return "ZAHL ={" + tokenizer.nval + "}"; }
 	else if (token == StreamTokenizer.TT_EOF) { return "EOF"; }
 	else if (token == StreamTokenizer.TT_WORD) { return "WORT ={" + tokenizer.sval + "}"; }
-	else { return "SONDERZEICHEN ={" + String.valueOf((char)token) + "}"; }
+	else { return "SONDERZEICHEN ={" + string.valueOf((char)token) + "}"; }
 }
 
 /// Liest das nächste Token ein
-void next() throws IOException {
+void Formelparser::next() {
 	lookahead = tokenizer.nextToken();    // kann sein: TT_EOF (Dateiende), TT_NUMBER (Zahl), TT_WORD (sonstiges)
 }
 
 /// Prüft, ob das nächste Token das erwartete ist
-void match(int expected) throws IOException {
+void Formelparser::match(int expected) {
 	if (lookahead != expected) {
-		throw new IOException("Erwartet: '" + tokenToString(expected) + "', erhalten: '" + tokenToString(lookahead) + "'");    //* Exception auf spezifischere ParseException umbauen
+		throw "Erwartet: '" + tokenToString(expected) + "', erhalten: '" + tokenToString(lookahead) + "'";    //* Exception auf spezifischere ParseException umbauen
 	} else {
 		next();
 	}
@@ -96,51 +100,51 @@ void match(int expected) throws IOException {
 
 
 /// Parst die Ableitungsregel: Expr -> Term (("+" | "-") Term)*
-double parseExpr() throws IOException {
+double Formelparser::parseExpr() {
 	double v, w;
 	int op;
 	
-	System.out.print("\n#parseExpr:");
+	cout << endl<<"#parseExpr:";
 	v = parseTerm();
 	while ((op = lookahead) == '+' || op == '-') {
 		next();
 		w = parseTerm();
-		System.out.print(" " + v + " " + (char)op + " " + w + " ");
+		cout << " " << v << " " << op << " " << w << " ";
 		if (op == '+') {
 			v += w;
 		} else if (op == '-') {
 			v -= w;
 		}
-		System.out.print("= " + v + " ");
+		cout << "= " << v << " ";
 	}
-	System.out.println("");
+	cout<<endl;
 	return v;
 }
 
 /// Parst die Ableitungsregel: Term -> Fact (("*" | "/") Fact)*
-double parseTerm() throws IOException {
+double Formelparser::parseTerm() {
 	double v, w;
 	int op;
 
-	System.out.print("\n#parseTerm:");
+	cout << endl<<"#parseTerm:";
 	v = parseFact();
 	while ((op = lookahead) == '*' || op == '/') {
-//            System.out.print(" " + v + " " + (char)op + " ");
+//            cout << " " << v << " " << (char)op << " ";
 		next();
 		w = parseTerm();
-		System.out.print(" " + v + " " + (char)op + " " + w + " ");
+		cout << " " << v << " " << op << " " << w << " ";
 		if (op == '*') {
 			v *= w;
 		} else if (op == '/') {
 			v /= w;
 		}
 	}
-	System.out.println("= " + v + " ");
+	cout<<endl<<"= " << v << " ";
 	return v;
 }
 
-/// Parst die Ableitungsregel: Fact -> (("cos" | "sin" | "tan") Trig)* | Trig
-double parseFact() throws IOException {
+/// Parst die Ableitungsregel: Fact -> (("c" | "s" | "t") Trig)* | Trig
+double Formelparser::parseFact() {
 	double v, w;
 	StringBuffer op = new StringBuffer();
 
@@ -148,52 +152,52 @@ double parseFact() throws IOException {
 	if (lookahead == StreamTokenizer.TT_WORD) {
 //			while (lookahead == StreamTokenizer.TT_WORD) {
 			op.replace(0, 3, tokenizer.sval);
-			if (op.toString().equals("cos") || op.toString().equals("sin") || op.toString().equals("tan")) {
-				System.out.print("\n#parseFact: ");
+			if (op='c' || op='s' || op='t') {	//* Stringvergleich
+				cout << endl<<"#parseFact: ";
 				next();
 				w = parseFact();
-				System.out.print(op + " " + w + " ");
-				if (op.toString().equals("cos")) {
-					v = java.lang.Math.cos(w);
-				} else if (op.toString().equals("sin")) {
-					v = java.lang.Math.sin(w);
-				} else if (op.toString().equals("tan")) {
-					v = java.lang.Math.tan(w);
+				cout << op << " " << w << " ";
+				if (op='c') {
+					v = cos(w);
+				} else if (op='s') {
+					v = sin(w);
+				} else if (op='t') {
+					v = tan(w);
 				}
 			} else {
-				throw new IOException("Unerwartetes Symbol: " + tokenToString(lookahead));
+				throw "Unerwartetes Symbol: " + tokenToString(lookahead);
 			}
 //			}
 	} else {
 		v = parseTrig();
 	}
-	System.out.println("= " + v + " "); 
+	cout<<endl<<"= " << v << " "; 
 	return v;
 }
 
 /// Parst die Ableitungsregel: Trig -> "(" Expr ")" | Number
-double parseTrig() throws IOException {
+double Formelparser::parseTrig() {
 	double v = 0.0;
 
-	System.out.print("\n#parseTrig: ");        
+	cout << endl<<"#parseTrig: ";        
 	if (lookahead == '(') {
 		match('(');
 		v = parseExpr();
-		System.out.print(" ( " + v + " ) ");
+		cout << " ( " << v << " ) ";
 		match(')');
 	} else if (lookahead == StreamTokenizer.TT_NUMBER) {
 		v = tokenizer.nval;
 		next();
 	} else {
-		throw new IOException("Unerwartetes Symbol: " + tokenToString(lookahead));
+		throw "Unerwartetes Symbol: " + tokenToString(lookahead);
 	}
-	System.out.println("=" + v + " ");
+	cout<<endl<<"=" << v << " ";
 	return v;
 }
 
 /// Parst übergebenen Ausdruck und gibt dessen Ergebnis zurrück
-double parse(String s) throws IOException {
-	System.out.println("parse(" + s + ")");
+double Formelparser::parse(string s) {
+	cout<<endl<<"parse(" << s << ")";
 	double v;
 	StringReader r = new StringReader(s);
 	tokenizer = new StreamTokenizer(r);
@@ -204,9 +208,9 @@ double parse(String s) throws IOException {
 	if (lookahead != StreamTokenizer.TT_EOF) {
 		isValid=FALSE;
 		parseResult = 0;
-		throw new IOException("Unerwartetes Symbol '" + (char)lookahead + "'");
+		throw "Unerwartetes Symbol '" + (char)lookahead + "'";
 	} else {
-//            System.out.print("\nDer arithmetische Ausdruck ist in Ordnung und ergibt: " + v);
+//            cout << endl<<"Der arithmetische Ausdruck ist in Ordnung und ergibt: " << v;
 		isValid=TRUE;
 		parseResult=v;
 	}
@@ -215,7 +219,7 @@ double parse(String s) throws IOException {
 
 
 /// Initialisiert die Membervariablen
-void FormelparserMinimal() {
+Formelparser::Formelparser() {
 	isValid = FALSE;
 	parseResult = 0;
 }
@@ -223,27 +227,25 @@ void FormelparserMinimal() {
 
 
 /// Ruft Parse() mit den Kommandozeilenparametern auf und meldet aufgetretene Fehler.
-void main (String args[]) throws IOException {
-	FormelparserMinimal fp = new FormelparserMinimal();
+int main (int argc, char* argv[]) {
+	Formelparser fp = new Formelparser();
 	double result;
 	
-	if (args.length < 1) {
-		System.err.println("Verwendung: java Formelparser\"<Ausdruck>\"");
-		System.err.println("Unterstützte Rechenoperationen: + - * / cos() sin() tan() einschließlich Klammern '()'.");
-		return;											//* Hier sollte ein Fehlercode zurückgeben werden
+	if (argc <= 1) {
+		cerr << endl << "Verwendung: Formelparser.out\"<Ausdruck>\"";
+		cerr << endl << "Unterstützte Rechenoperationen: + - * / cos() sin() tan() einschließlich Klammern '()'.";
+		return 1;											//* Hier sollte ein Fehlercode zurückgeben werden
 	}
 	
 	try {
-		System.out.println();
-		result = fp.parse(args[0]);
-		System.out.println();
-		System.out.println(args[0] + " = " + Double.toString(result));
+		cout<<endl;
+		result = fp.parse(argv[0]);
+		cout<<endl;
+		cout<<endl<<argv[0] << " = " << result;
+		return 0;
 		
-	} catch (IOException e1) {
-		System.err.println("!!! Es ist ein Fehler aufgetreten: " + e1.getMessage() + " !!!");
-		//* Hier sollte ein Fehlercode zurückgeben werden
-	} finally {
-//            System.out.println(args[0] + "=" + fp.parseResult + "; gültig: "+ fp.isValid);
-		System.out.println();
+	} catch (const char* p) {
+		cerr << endl << "!!! Es ist ein Fehler aufgetreten: " << p << " !!!";
+		return 2;	//* Hier sollte ein Fehlercode zurückgeben werden
 	}
 }
